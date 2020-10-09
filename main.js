a76b81441101fc3153aba0bfccb8eb9a306bb653
Ellie Brayton <ebrayton@ufl.edu>
2020-10-09 16:25:11 -0500

    Finished part ii - iv

--- main.js
+++ main.js
@@ -19,12 +19,28 @@ for (i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener("mouseout", highlight);
 }
 
-//PART II
+//PART II - IV
 
 var boxOneMessage = document.createElement("p")
 boxOneMessage.innerHTML = "Oooh - so close, but no cigar"
 document.getElementById("one").addEventListener("click",
 function(){
     document.getElementById("one").appendChild(boxOneMessage);
+}, false
+);
+
+var boxTwoMessage = document.createElement("p")
+boxTwoMessage.innerHTML = "DING DING DING - We have a winner"
+document.getElementById("two").addEventListener("click",
+function(){
+    document.getElementById("two").appendChild(boxTwoMessage);
+}, false
+);
+
+var boxThreeMessage = document.createElement("p")
+boxThreeMessage.innerHTML = "Oops, butter luck next time"
+document.getElementById("three").addEventListener("click",
+function(){
+    document.getElementById("three").appendChild(boxThreeMessage);
 }, false
 );
